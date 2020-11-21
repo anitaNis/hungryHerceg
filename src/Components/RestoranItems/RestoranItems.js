d67f7b54515e1c2bc97284b6ac659e1a5162c074
anitaNis <anita_t2001@yahoo.com>
2020-11-21 12:17:04 +0100

    anita stil

--- src/Components/RestoranItems/RestoranItems.js
+++ src/Components/RestoranItems/RestoranItems.js
@@ -11,7 +11,7 @@ const RestoranItems = ({ items }) => {
           <img src={img} alt={title} className='photo' />
           <div className="item-info">
             <header>
-              <h4>{title}</h4>
+              <h4 className='restaurantItem-title'>{title}</h4>
               <h4 className="price">${price}</h4>
             </header>
             <p className="item-text">{desc}</p>
