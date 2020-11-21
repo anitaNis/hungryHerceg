13d1a6a5b78405432855ab0cde1ebc754eafccdd
anitaNis <anita_t2001@yahoo.com>
2020-11-21 17:57:22 +0100

    about

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,27 +1,29 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import AllPolls from '../AllPolls/AllPolls';
-import CreateRestaurant from "../CreateRestaurant/CreateRestaurant"
-import CreateRestaurantDatabase from "../CreateRestaurantDatabase/CreateRestaurantDatabase"
+import CreateRestaurant from "../CreateRestaurant/CreateRestaurant";
+import CreateRestaurantDatabase from "../CreateRestaurantDatabase/CreateRestaurantDatabase";
 import ViewOrder from '../ViewOrder/ViewOrder';
-import "./style.css"
+import "./style.css";
+import About from '../About/About';
 
 const Home = () => {
     return (
 
         <div className="main">
             <Link className="pollButton" to="/polls">Active Polls</Link>
-            <Link  className="pollButton" to="/create-poll">Create Poll</Link>
-            <Link  className="pollButton" to="/orders">Active Orders</Link>
-            <Link  className="pollButton" to="/create-order">Create Order</Link>
+            <Link className="pollButton" to="/create-poll">Create Poll</Link>
+            <Link className="pollButton" to="/orders">Active Orders</Link>
+            <Link className="pollButton" to="/create-order">Create Order</Link>
             {/* <CreateRestaurant />
         <CreateRestaurantDatabase /> */}
             {/* active polls true */}
             <AllPolls />
-            {/* <ViewOrder /> */}
+            <ViewOrder />
+            {/* <About /> */}
         </div>
 
     );
-}
+};
 
 export default Home;
